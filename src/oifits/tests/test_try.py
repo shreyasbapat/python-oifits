f0b3e41e4c477834bbfcf860d37ac2b7e36bb69c
Shreyas Bapat <bapat.shreyas@gmail.com>
2019-09-06 03:37:56 +0530

    Add a test

---
+++ src/oifits/tests/test_try.py
@@ -0,0 +1,7 @@
+from oifits.read import OIParser
+
+def test_everything_works():
+    a = OIParser.read("test.oifits")
+    oidata = a.data
+    wav = oidata.wavelength['WAVELENGTH_NAME'].eff_wave[0]
+    assert wav == 0.0013189458986744285
